ca2c80fe03e205d5dcd802ee982414ce45941165
fsclaudio <fsclaudio@gmail.com>
2023-07-18 13:26:19 -0300

    add create table orders

--- internal/infra/database/order_repository.go
+++ internal/infra/database/order_repository.go
@@ -3,7 +3,7 @@ package database
 import (
 	"database/sql"
 
-	"github.com/devfullcycle/go-intensivo-jul/internal/entity"
+	"https://github.com/fsclaudio/go-cfs/internal/entity"
 )
 
 type OrderRepository struct {
@@ -17,6 +17,15 @@ func NewOrderRepository(db *sql.DB) *OrderRepository {
 }
 
 func (r *OrderRepository) Save(order *entity.Order) error {
+	// Create table
+	statement, err := r.DB.Prepare("CREATE TABLE IF NOT EXISTS orders (id varchar(255) PRIMARY KEY NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL)")
+	if err != nil {
+		log.Println("Error in creating table")
+	} else {
+		log.Println("Successfully created table orders!")
+	}
+	statement.Exec()
+
 	_, err := r.Db.Exec("INSERT INTO orders (id, price, tax, final_price) VALUES (?,?,?,?)",
 		order.ID, order.Price, order.Tax, order.FinalPrice)
 	if err != nil {
